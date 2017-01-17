f684698c783ef5d4eeba9ddfd9cb501087243c04
Rich Neswold <neswold@fnal.gov>
2017-01-17 08:52:43 -0600

    Pull the trunknode type out of server.h and place it in its own header. The "large message" support needs this definition, but doesn't need the rest of the content of server.h.

--- server.h
+++ server.h
@@ -155,31 +155,11 @@ class nodename_t {
     uint32_t raw() const { return h; }
 };
 
-class trunknode_t {
-    uint16_t tn;
-
- public:
-    trunknode_t() : tn(0) {}
-    trunknode_t(uint8_t const t, uint8_t const n) :
-	tn((uint16_t(t) << 8) | uint16_t(n)) {}
-    trunknode_t(trunknode_t const &obj) : tn(obj.tn) {}
-    explicit trunknode_t(uint16_t const addr) : tn(addr) {}
-
-    friend uint32_t unique_node_key(trunknode_t, uint16_t);
-
-    bool operator< (trunknode_t const o) const { return tn < o.tn; }
-    bool operator== (trunknode_t const o) const { return tn == o.tn; }
-    bool operator!= (trunknode_t const o) const { return tn != o.tn; }
-
-    bool isBlank() const { return tn == 0; }
-    uint8_t trunk() const { return uint8_t(tn >> 8); }
-    uint8_t node() const { return uint8_t(tn); }
-    uint16_t raw() const { return tn; }
-};
+#include "trunknode.h"
 
 inline uint32_t unique_node_key(trunknode_t n, uint16_t m)
 {
-    return (uint32_t(n.tn) << 16) | uint32_t(m);
+    return (uint32_t(n.raw()) << 16) | uint32_t(m);
 }
 
 // Acnet header macros
